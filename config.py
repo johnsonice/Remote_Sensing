524002f4cf15b8ab50a6ac5936e96a54b49b5748
Chengyu <huangchengyu16@gmail.com>
2022-05-06 15:21:17 -0400

    add

--- config.py
+++ config.py
@@ -7,6 +7,15 @@ Created on Fri May  6 13:43:35 2022
 """
 
 ## config 
+import os
+user = os.uname()[1]
 
+if user =='chengyu-desktop':
+    DATA_ROOT = '/media/chengyu/Elements1/remote_sensing/data/'
+else:
+    DATA_ROOT = './data'
+    
+    
 MAJOR_STATES = [5, 17, 18, 19, 20, 27, 29, 31, 38, 39, 46]
-MODIS_DATA_FOLDER = '/media/chengyu/Elements1/remote_sensing/data/MODIS'
+
+MODIS_DATA_FOLDER = os.path.join(DATA_ROOT,'MODIS')
